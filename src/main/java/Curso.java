bcb80b019fecdb4c0d65ebf7db13ce1b5cdf605f
Arthur Lisboa <lisboa.arthur159@gmail.com>
2022-03-23 07:51:32 -0300

    code commit

---
+++ src/main/java/Curso.java
@@ -0,0 +1,25 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Curso extends Conteudo{
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Curso() {
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
---
+++ src/main/java/Mentoria.java
@@ -0,0 +1,27 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+public class Mentoria extends Conteudo{
+
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public Mentoria() {
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
